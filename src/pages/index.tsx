6ba161277a22bd88bfc2f6be1384aa4745d2f50f
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-27 20:02:36 +0300

    Add db and connect with prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,32 @@ import { Button } from "~/components/ui";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data, refetch, isFetching } = api.example.getAll.useQuery(undefined, {
+    enabled: false,
+  });
 
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold">Washing Machine UI App</h1>
+        <h1 className="mb-6 text-4xl font-bold">Washing Machine UI App</h1>
 
         <div>
-          <Button>{hello.data?.greeting ?? "Loading..."}</Button>
+          <Button onClick={() => void refetch()}>
+            Click me to fetch examples
+          </Button>
         </div>
+
+        {isFetching ? (
+          <p className="mt-8 text-center">Loading...</p>
+        ) : (
+          data && (
+            <ul className="mt-8">
+              {data.map((example) => (
+                <li key={example.id}>{example.id}</li>
+              ))}
+            </ul>
+          )
+        )}
       </main>
     </>
   );
