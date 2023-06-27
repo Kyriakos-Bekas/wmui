82ebef53358a64513f1788d655bf0eee043e26c6
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-27 19:26:10 +0300

    Modify configuration for components and add Button component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,22 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Washing Machine UI App</title>
+        <meta
+          name="description"
+          content="This UI simplifies interacting with your washing machine"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
