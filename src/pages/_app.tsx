0d65fc8c893bc4964060a0e7499ae2f0c799bfaa
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-27 20:41:10 +0300

    Add ThemeProvider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import { type AppType } from "next/app";
-
+import Head from "next/head";
+import { Navbar, ThemeProvider } from "~/components";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
@@ -16,7 +16,15 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <div className="grid min-h-screen grid-rows-app-layout">
+          <Navbar />
+          <div className="container">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 };
