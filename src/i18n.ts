aee82857a70b3f898bf8a8388597f9c3bbadd44d
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-29 00:36:57 +0300

    Update homepage and i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,11 +4,22 @@ import { type DEFAULT_WASHING_PROGRAMS } from "./utils/types";
 type DefaultWashingProgramKeys =
   (typeof DEFAULT_WASHING_PROGRAMS)[number]["slug"];
 
+type HomepageSection = "presets" | "favorites";
+
 type UiText = {
   navbar: {
     help: string;
     contact: string;
   };
+  homepage: Record<
+    HomepageSection,
+    {
+      title: string;
+      description: string;
+      empty: string;
+    }
+  >;
+  loading: string;
   toggleLocale: string;
   toggleTheme: string;
   defaultPrograms: Record<DefaultWashingProgramKeys, string>;
@@ -20,6 +31,21 @@ export const i18n: Record<AvailableLocales, UiText> = {
       help: "Help",
       contact: "Contact",
     },
+    homepage: {
+      favorites: {
+        title: "Favorites",
+        description:
+          "Your saved programs will be listed here. Press and hold on a program to start it immediately",
+        empty: "You don't have any favorite programs yet",
+      },
+      presets: {
+        title: "Presets",
+        description:
+          "A collection of pre-configured programs for different types of clothing",
+        empty: "Sorry, we couldn't load the presets. Please try again later",
+      },
+    },
+    loading: "Loading...",
     toggleLocale: "Toggle Locale",
     toggleTheme: "Toggle Theme",
     defaultPrograms: {
@@ -42,6 +68,22 @@ export const i18n: Record<AvailableLocales, UiText> = {
       help: "Βοήθεια",
       contact: "Επικοινωνία",
     },
+    homepage: {
+      favorites: {
+        title: "Αγαπημένα",
+        description:
+          "Τα αγαπημένα σας προγράμματα θα εμφανίζονται εδώ. Πατήστε και κρατήστε πατημένο ένα πρόγραμμα για να το ξεκινήσετε αμέσως",
+        empty: "Δεν έχετε ακόμα αγαπημένα προγράμματα",
+      },
+      presets: {
+        title: "Προεπιλογές",
+        description:
+          "Μια συλλογή προκαθορισμένων προγραμμάτων για διαφορετικούς τύπους ρούχων",
+        empty:
+          "Λυπούμαστε, δεν μπορέσαμε να φορτώσουμε τις προεπιλογές. Παρακαλώ προσπαθήστε ξανά αργότερα",
+      },
+    },
+    loading: "Φόρτωση...",
     toggleLocale: "Αλλαγή Γλώσσας",
     toggleTheme: "Αλλαγή Θέματος",
     defaultPrograms: {
