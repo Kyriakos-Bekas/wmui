7386d01d223577fb6a06c462eb2aa69cb146a055
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-30 02:35:32 +0300

    Add custom program creation and deletion mutations

--- src/server/api/routers/program.ts
+++ src/server/api/routers/program.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { slugify } from "~/utils/slugify";
 
 export const programRouter = createTRPCRouter({
   getAll: publicProcedure
@@ -21,5 +21,44 @@ export const programRouter = createTRPCRouter({
       }
 
       return ctx.prisma.program.findMany();
+    }),
+  createCustom: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        temperature: z.number(),
+        spin: z.number(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { name, temperature, spin } = input;
+
+      return ctx.prisma.program.create({
+        data: {
+          name,
+          type: "CUSTOM",
+          temperature,
+          spin,
+          duration: Math.floor(Math.random() * 90 + 30),
+          inProgress: false,
+          start: "",
+          slug: slugify(name),
+        },
+      });
+    }),
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { id } = input;
+
+      return ctx.prisma.program.delete({
+        where: {
+          id,
+        },
+      });
     }),
 });
