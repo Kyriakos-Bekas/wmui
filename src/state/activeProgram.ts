bbaf632ce755ddbe4fd17c857119861b7e4af135
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-06-30 02:37:06 +0300

    Add state management for active program

---
+++ src/state/activeProgram.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { type AllowedSpin, type AllowedTemperatures } from "~/utils/types";
+
+type ActiveProgramStore = {
+  slug: string;
+  temperature: AllowedTemperatures;
+  spin: AllowedSpin;
+  setActiveProgram: ({
+    slug,
+    temperature,
+    spin,
+  }: {
+    slug: string;
+    temperature: AllowedTemperatures;
+    spin: AllowedSpin;
+  }) => void;
+};
+
+export const useActiveProgramStore = create<ActiveProgramStore>((set) => ({
+  slug: "",
+  temperature: 20,
+  spin: 100,
+  setActiveProgram: ({ slug, temperature, spin }) =>
+    set(() => ({ slug, temperature, spin })),
+}));
