d9dfbe8cd757bfe92fd7251baaed8ab3d373576a
Kyriakos Bekas <kyriakosbekas00@gmail.com>
2023-07-01 02:13:28 +0300

    Add getOne query and update mutation for programs
    Also add check for already existing name with localized error message

--- src/server/api/routers/program.ts
+++ src/server/api/routers/program.ts
@@ -1,8 +1,41 @@
+import { type Program } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { slugify } from "~/utils/slugify";
 
+const checkIfNameExists = (
+  name: string,
+  programWithMatchingName: Program | null,
+  id?: string
+) => {
+  if (!!programWithMatchingName && programWithMatchingName?.id !== id) {
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+      message: JSON.stringify({
+        en: `A program with the name '${name}' already exists. Please choose another name and try again`,
+        gr: `Υπάρχει ήδη πρόγραμμα με το όνομα '${name}'. Παρακαλούμε επιλέξτε ένα άλλο όνομα και προσπαθήστε ξανά`,
+      }),
+    });
+  }
+};
+
 export const programRouter = createTRPCRouter({
+  getOne: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(({ ctx, input }) => {
+      const { id } = input;
+
+      return ctx.prisma.program.findUnique({
+        where: {
+          id,
+        },
+      });
+    }),
   getAll: publicProcedure
     .input(
       z.object({
@@ -30,8 +63,16 @@ export const programRouter = createTRPCRouter({
         spin: z.number(),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       const { name, temperature, spin } = input;
+
+      const nameExists = await ctx.prisma.program.findFirst({
+        where: {
+          name,
+        },
+      });
+
+      checkIfNameExists(name, nameExists);
 
       return ctx.prisma.program.create({
         data: {
@@ -58,6 +99,54 @@ export const programRouter = createTRPCRouter({
       return ctx.prisma.program.delete({
         where: {
           id,
+        },
+      });
+    }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        temperature: z.number(),
+        spin: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, name, temperature, spin } = input;
+
+      const program = await ctx.prisma.program.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!program) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: JSON.stringify({
+            en: `Program with id '${id}' does not exist`,
+            gr: `Το πρόγραμμα με id '${id}' δεν υπάρχει`,
+          }),
+        });
+      }
+
+      const nameExists = await ctx.prisma.program.findFirst({
+        where: {
+          name,
+        },
+      });
+
+      checkIfNameExists(name, nameExists, id);
+
+      return ctx.prisma.program.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          temperature,
+          spin,
+          slug: slugify(name),
         },
       });
     }),
